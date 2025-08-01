d10aee54a6188bcd270c86121d8dba2b5f52c572
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-01 17:16:37 +0530

    Add <cube of a float number> Program

---
+++ day1/Cubeofnum.java
@@ -0,0 +1,13 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Cubeofnum {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            float a = sc.nextFloat();
+            float cube = a*a*a;
+            System.out.println("cube is :"+ cube);
+
+        }
+    }
---
+++ out/production/java-daily-practice1/README.md
@@ -0,0 +1,8 @@
+# java-daily-practice1
+“This repo contains my daily Java practice : basics (conditionals,loops)."
+## Structure
+- `basics/` : Conditionals, loops, etc.
+
+
+- '01 august 2025' Add first,SumOfTwo
+ 
