59b260916a38c7682e19a778aa5286c6541adc17
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-01 14:50:57 +0530

    Fristcode

---
+++ beginning/First.java
@@ -0,0 +1,14 @@
+package beginning;
+
+public class First {
+    public static void main(String[] args) {
+        String a = "Ankit upadhyay";
+        int b = 20;
+        float c = 74.23f;
+        System.out.println("Name:-" + a);
+        System.out.println("age:-"+ b);
+        System.out.println("Aggergate percentage:-"+c+"%");
+
+    }
+    
+}
