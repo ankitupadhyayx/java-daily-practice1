28bde2359e9b5f51a45373861eecb383370e91b6
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-01 17:24:45 +0530

    Add <Largest num among three

---
+++ day1/Largestnum.java
@@ -0,0 +1,19 @@
+package day1;
+import java.util.Scanner;
+public class Largestnum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write 3 values to compare");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a > b  &&  a>c){
+            System.out.println(" a is great");
+        } else if (b>c) {
+            System.out.println("b is great");
+        }else{
+            System.out.println("c is great");
+        }
+
+    }
+}
