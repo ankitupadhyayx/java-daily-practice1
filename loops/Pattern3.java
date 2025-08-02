d2b1ffbfaacd86b2229855608b29b585e767e1d6
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-02 16:19:35 +0530

    Add <Patter3 > Program

---
+++ loops/Pattern3.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 0 ; i < 4 ; i ++){
+            for(int j = 0; j < 4 ; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
+// *****
+// *****
+// *****
+// *****
