06556a0f5c802e661c67abe9a02baf6131f50f27
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-02 16:07:10 +0530

    Add <Patter2 >Program

---
+++ loops/Pattern2.java
@@ -0,0 +1,14 @@
+package loops;
+public class Pattern2 {
+    public static void main(String[] args) {
+        int noOfLines = 5;
+        for(int line =1 ; line<=noOfLines ; line++ ){
+            for(int space = noOfLines; space > line; space--) {
+                System.out.print(" ");
+            }
+            for (int star =1;star<=noOfLines;star++){
+            System.out.print("*");}
+            System.out.println();
+        }
+    }
+}
