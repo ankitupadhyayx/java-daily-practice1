26b420e7adcdd92b3ea7cccff994b64cd48b6e52
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-02 22:21:34 +0530

    Add <Pattern6> program

--- loops/Pattern5.java
+++ loops/Pattern5.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pattern5 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("enter the number of line");
+    System.out.print("enter the number of line:");
     int n = sc.nextInt();
         for(int i = 1 ; i <= n ; i++){
             for(int j = 0 ; j < i ; j++){
