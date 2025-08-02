dc011773c11e3ffd9b7d9f2b2d96408e242bdbed
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-02 21:31:18 +0530

    Add <pattern5> Program

--- loops/Pattern4.java
+++ loops/Pattern4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Pattern4{
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the number of lines for the pattern:");
+    System.out.print("Enter the number of lines for the pattern:");
     int n = scanner.nextInt();
 
     for(int i =0 ; i < n ; i++){
---
+++ loops/Pattern5.java
@@ -0,0 +1,26 @@
+package loops;
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter the number of line");
+    int n = sc.nextInt();
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < (n*2 - (2*i-1)); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// n=5
