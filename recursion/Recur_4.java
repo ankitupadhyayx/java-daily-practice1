5922c3fc2a7b74099b6f3f92d05a32bf2699acd6
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-05 18:28:35 +0530

    update Recur_4.java Palindrome number

--- recursion/Recur_4.java
+++ recursion/Recur_4.java
@@ -1,3 +1,18 @@
+package recursion;
+
 public class Recur_4 {
-    
+    static boolean palin(int num , int org , int reverse){
+        if(num==0){
+            return org  == reverse ;
+        }
+        reverse = reverse*10+num%10;
+        return palin(num/10,org,reverse);
+    }
+    //static int palindrome(int num){}
+
+    public static void main(String[] args) {
+        boolean result = palin(151,152,0);
+        System.out.println(result ? "palindrome " : "not");
+    }
 }
+//palindrome number
