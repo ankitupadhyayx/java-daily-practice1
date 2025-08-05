4d63ed3fbd83ed12b7ed86edcfe8d3e5516ba134
Ankit upadhyay <ankitupadhyayx@gmail.com>
2025-08-05 18:06:12 +0530

    Recur_2.java

--- recursion/Recur_2.java
+++ recursion/Recur_2.java
@@ -1,3 +1,16 @@
+package recursion;
 public class Recur_2 {
-    
+    static int fact(int num){
+        if(num==1){
+            return 1;
+        }
+        int smallResult=fact(num-1);
+        return smallResult*num;
+    }
+
+    public static void main(String[] args) {
+        int r = fact(5);
+        System.out.println(r);
+    }
 }
+//Write a recursive function to return the factorial of a given integer
